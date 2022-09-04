5c6963a9ae0fccd287ceee516f9a9b550343f2c2
Yunseonghwan <sunghwan0208@naver.com>
2022-09-04 19:43:00 +0900

    refactoring 6-2

---
+++ chapter/chapter6/6-2.js
@@ -0,0 +1,14 @@
+function rating(aDriver) {
+  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(aDriver) {
+  return aDriver.numberOfLateDeliveries > 5;
+}
+
+function reportLines(aCustomer) {
+  const lines = [];
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
+  return lines;
+}
