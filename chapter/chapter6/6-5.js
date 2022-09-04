4968663babff54f53bb07e936c3b9170b6fbf60e
Yunseonghwan <sunghwan0208@naver.com>
2022-09-04 20:36:04 +0900

    refactoring 6-5

---
+++ chapter/chapter6/6-5.js
@@ -0,0 +1,24 @@
+// /* 함수이름 바꾸기 */
+
+// function circum(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// //이름 바꾸기
+// function circumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// /* 함수 이름 바꾸기(마이그레이션 절차) */
+
+// function circum(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// function circum(radius) {
+//   return circumference(radius);
+// }
+
+// function circumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
